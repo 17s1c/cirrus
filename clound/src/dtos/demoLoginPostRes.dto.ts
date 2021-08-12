0690320ddf732ede2fe6ac24dbaeeb5c0c798883
nekotrek <creamidea@gmail.com>
2021-08-12 23:24:16 +0800

    chore: move out the cloud directory
    move to https://github.com/17s1c/decorate-framework

--- clound/src/dtos/demoLoginPostRes.dto.ts
+++
@@ -1,9 +0,0 @@
-import { Exclude, Expose } from 'class-transformer'
-import { IsNumber } from 'class-validator'
-
-@Exclude()
-export class DemoLoginPostResDto {
-  @Expose()
-  @IsNumber()
-  readonly token: number
-}
